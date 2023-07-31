b258d667a9881b80ee2d4e9e9bd7a50950d24cf4
experiment1234 <experiment123@naver.com>
2023-07-31 14:50:04 +0900

    20230731

---
+++ 13_Java_Api/src/ex03_date_time/Ex03_java_sql_Date.java
@@ -0,0 +1,23 @@
+package ex03_date_time;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Ex03_java_sql_Date {
+
+  public static void main(String[] args) {
+  
+    // java.sql.Date 클래스 
+    // DB의 DATE타입을 처리하는 Date 클래스
+    
+    // 현재 날짜
+    Date date = new Date(System.currentTimeMillis());
+    System.out.println(date);
+    
+    // java.sql.Timestamp 클래스 
+    // DB의 TIMESTAMP 타입을 처리하는 Timestamp
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    System.out.println(timestamp);
+  }
+
+}
